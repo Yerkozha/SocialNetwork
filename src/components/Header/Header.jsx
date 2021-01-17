a603865f4fe31987ad1f941b40d77676ef54816b
Yerkozha <ergoja2211@gmail.com>
2021-01-17 19:40:20 +0600

    Add all files

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from './Header.module.css'
+
+
+
+const Header = () => {
+    return (
+        <div className={s.header}>
+            <img src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.logaster.ru%2F&psig=AOvVaw3s95PpteA7x4Szen8vebIS&ust=1610892141414000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLCt2YHPoO4CFQAAAAAdAAAAABAD'></img>
+        </div>
+    );
+}
+
+export default Header;
