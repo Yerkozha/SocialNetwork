f5061f336c1fc5c51c299f5b4c6e591b9b292afe
Yerkozha <ergoja2211@gmail.com>
2021-01-18 19:06:13 +0600

    changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import s from './Header.module.css'
-
-
+import s from './Header.module.css';
 
 const Header = () => {
-    return (
-        <div className={s.header}>
-            <img src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.logaster.ru%2F&psig=AOvVaw3s95PpteA7x4Szen8vebIS&ust=1610892141414000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLCt2YHPoO4CFQAAAAAdAAAAABAD'></img>
-        </div>
-    );
+    return <header className={s.header}>
+        <img src='https://www.freelogodesign.org/Content/img/logo-ex-7.png' />
+    </header>
 }
 
 export default Header;
